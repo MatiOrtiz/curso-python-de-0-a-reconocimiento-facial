4a02eda2bc3145edeeddbb162f231637086d0999
Matías Nicolás Ortiz <88355010+MatiOrtiz@users.noreply.github.com>
2024-06-30 14:31:20 -0300

    actividades agregadas

---
+++ conjuntosEnPy.py
@@ -0,0 +1,27 @@
+A={1,2,3,4}
+B={2,3,5,6}
+C={3,4,6,7}
+
+#Equivalencia de Conjuntos
+print("Los conjuntos son equivalentes:")
+print(A==B)
+print(A==C)
+print(B==C)
+
+#Unión de Conjuntos
+print("Las uniones de conjunto son: ")
+print(A|B)
+print(A|C)
+print(B|C)
+
+#Intersección de Conjuntos
+print("Las intersecciones de conjunto son: ")
+print(A&B)
+print(A&C)
+print(B&C)
+
+#Diferencia simétrica de Conjuntos
+print("Las diferencias simétricas de conjunto son: ")
+print(A^B)
+print(A^C)
+print(B^C)
---
+++ sumatoriaDe0A100.py
@@ -0,0 +1,5 @@
+suma=0
+
+for i in range(101):
+    suma+=i
+    print(f"La sumatoria de los numeros es {suma}")
